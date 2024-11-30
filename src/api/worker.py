e93e827ece95991ae8874c7f0aeb00c0f901fcd7
RMxNinja <91172425+RupeshMangalam21@users.noreply.github.com>
2024-11-30 17:40:31 +0530

    added worker

---
+++ src/api/worker.py
@@ -0,0 +1,15 @@
+import os
+import redis
+from rq import Worker, Queue, Connection
+from api.execution import execute_code
+
+# Redis connection settings
+redis_url = os.getenv('REDIS_URL', 'redis://redis:6379/0')
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        print("Worker is starting...")
+        q = Queue()  # Default queue
+        worker = Worker([q])
+        worker.work()
